a860312c4ea3d7659a20d7d8955ceb2cb6f411e2
bicklil <lemanjc@gmail.com>
2016-03-17 21:16:05 +0100

    ajout video

--- projet_c/main.c
+++ projet_c/main.c
@@ -17,11 +17,11 @@ typedef struct {int x;
 cercle* ajoutCercle(cercle* premier,cercle* actuel)
 // on ajoute les nouveux cercles en tete de chaine
 {
-    if(premier!=NULL)
+    if(premier != NULL)
     {
-        premier->precedent=actuel;
+        premier->precedent = actuel;
     }
-    actuel->suivant=premier;
+    actuel->suivant = premier;
     return actuel;
 }
 
@@ -30,18 +30,18 @@ cercle* supprimerCercle(cercle* actuel,cercle* premier)
 {
     if (actuel->precedent == NULL)
     {
-        (actuel->suivant)->precedent=NULL;
-        premier=actuel->suivant;
+        (actuel->suivant)->precedent = NULL;
+        premier = actuel->suivant;
         return premier;
     }
     if (actuel->suivant == NULL)
     {
-        (actuel->precedent)->suivant=NULL;
+        (actuel->precedent)->suivant = NULL;
         free(actuel);
         return premier;
     }
-    (actuel->suivant)->precedent=actuel->precedent;
-    (actuel->precedent)->suivant=actuel->suivant;
+    (actuel->suivant)->precedent = actuel->precedent;
+    (actuel->precedent)->suivant = actuel->suivant;
     return premier;
 
 }
@@ -60,7 +60,7 @@ IplImage* captureImage(cvCapture* capture)
 
 CvCapture* loadVideo()
 {
-    cvCapture* capture=NULL;
+    cvCapture* capture = NULL;
     while(!capture)
     {
         capture=cvCreateCameraCapture(); // mettre l'adresse de la video connait pas
@@ -84,10 +84,10 @@ cercle* createCircleRandomp(IplImage* image,cercle* premier)
 
 	cvCircle(image, cvPoint ( aleaLargeur, aleahauteur ) ,RAYON,cvScalar( 20, 148, 20,0 ),1,0,0);
 
-	ptcercle->x=aleaLargeur;
-	ptcercle->y=aleaHauteur;
-	ptcercle->rayon=RAYON;
-	ptcercle->couleur=cvScalar( 0x0, 0x0, 0x0,0 );
+	ptcercle->x = aleaLargeur;
+	ptcercle->y = aleaHauteur;
+	ptcercle->rayon = RAYON;
+	ptcercle->couleur = cvScalar( 0x0, 0x0, 0x0,0 );
 
 	return ajoutCercle(premier,ptcercle);
 
@@ -95,17 +95,17 @@ cercle* createCircleRandomp(IplImage* image,cercle* premier)
 
 void modifColorandsizeCircle(cercle* ptcercle)
   {
-      	cvScalar couleur[NBCOULEUR]=(cvScalar( 20, 148, 20,0 ),cvScalar( 179, 255, 49,0 ),
+      	cvScalar couleur[NBCOULEUR] = (cvScalar( 20, 148, 20,0 ),cvScalar( 179, 255, 49,0 ),
                        cvScalar( 213, 220, 51,0 ),cvScalar( 233, 185, 52,0 ),
                        cvScalar( 246, 149, 51,0 ),cvScalar( 252, 107, 51,0 ),
                        cvScalar( 254, 107, 51,0 ),cvScalar( 254, 50, 49,0 ),
                        cvScalar( 0x0, 0x0, 0x0,0 ),cvScalar( 0x0, 0x0, 0x0,0 ));
 
-        if((ptcercle->compteur%4)==0) // A METTRE EN TEST AU MOMENT DE L'INCREMENTATION
+        if((ptcercle->compteur%4) == 0) // A METTRE EN TEST AU MOMENT DE L'INCREMENTATION
         {
-            ptcercle->couleur=couleur[compteur/4];
+            ptcercle->couleur = couleur[compteur/4];
         }
-       ptcercle->rayon=RAYON-compteur/4;// reduction du cercle
+       ptcercle->rayon = RAYON-compteur/4;// reduction du cercle
   }
 
 int main(void)
@@ -116,19 +116,19 @@ int main(void)
 	cercle* premiercercle;
 
 	srand(time(NULL)); // initialisation de rand
-	ptvideo=loadVideo;
-	premiercercle=NULL;
+	ptvideo = loadVideo;
+	premiercercle = NULL;
 
     cvNamedWindow("Image originale", CV_WINDOW_AUTOSIZE);
 	cvMoveWindow("Image originale", 50, 500);
 
-	while(i<25)
+	while(i < 25)
     {
-        ptImage=captureImage(ptvideo);
-        premier=createCircleRandomp(ptImage,premier);
+        ptImage = captureImage(ptvideo);
+        premier = createCircleRandomp(ptImage,premier);
         cvShowImage("Image originale", ptImage);
         cvWaitKey(1000);
-        i=i+1;
+        i = i+1;
     }
 
 	cvReleaseImage(&ptImage);
